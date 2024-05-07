74f54a7441ef88b2d0268b74a7e4300ff280965d
Dmitriy Adieliev <d.adieliev@urancompany.com>
2024-05-07 17:19:33 +0300

    add internationalization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
+import astroI18next from "astro-i18next";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), astroI18next()],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["ua", "en"]
+  }
 });
